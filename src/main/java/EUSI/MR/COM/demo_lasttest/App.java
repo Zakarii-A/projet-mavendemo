f0a7c3899543b97b9f4312eda5fc310cf7da4e7d
zakaria <zakaria@gmail.com>
2025-01-04 17:14:50 +0100

    demot-pojetct-maven

---
+++ src/main/java/EUSI/MR/COM/demo_lasttest/App.java
@@ -0,0 +1,10 @@
+package EUSI.MR.COM.demo_lasttest;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/EUSI/MR/COM/demo_lasttest/AppTest.java
@@ -0,0 +1,19 @@
+package EUSI.MR.COM.demo_lasttest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
